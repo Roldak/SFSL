f430501a31bc58d116b57ce7e5042335ab115664
Romain <romain.beguet@epfl.ch>
2014-11-13 01:28:11 +0100

    some documentation

--- Compiler/Common/MemoryManager.h
+++ Compiler/Common/MemoryManager.h
@@ -1,34 +1,66 @@
 //
-//  Lexer.h
+//  MemoryManager.h
 //  SFSL
 //
 //  Created by Romain Beguet on 12.11.14.
 //  Copyright (c) 2014 Romain Beguet. All rights reserved.
 //
 
-#ifndef __SFSL__Lexer__
-#define __SFSL__Lexer__
+#ifndef __SFSL__MemoryManager__
+#define __SFSL__MemoryManager__
 
-#include "Tokens/Token.h"
+#include <vector>
+#include "MemoryManageable.h"
 
 namespace sfsl {
 
-namespace lex {
+namespace common {
 
-    class Lexer {
+    /**
+     * @brief The AbstractMemoryManager class
+     *
+     */
+    class AbstractMemoryManager {
     public:
 
-        Lexer(const std::string& input);
+        virtual ~AbstractMemoryManager() = 0;
 
-        bool hasMore();
-        tok::Token* getNext();
+        template<typename T, typename... Args>
+        T* New(Args... args) {
+            T* ptr = new T(args...);
+            push(ptr);
+            return ptr;
+        }
 
-    private:
+    protected:
 
-        const std::string& _input;
+        virtual void push(MemoryManageable* ptr) = 0;
+        virtual void free() = 0;
 
     };
 
+    template< template<typename> class Collection_t = std::vector >
+    class MemoryManager : public AbstractMemoryManager {
+    public:
+
+        MemoryManager() {}
+        virtual ~MemoryManager() { free(); }
+
+    private:
+
+        virtual void push(MemoryManageable *ptr) {
+            _allocated.push_back(ptr);
+        }
+
+        virtual void free() {
+            for (auto ptr : _allocated) {
+                delete ptr;
+            }
+        }
+
+        Collection_t<MemoryManageable*> _allocated;
+
+    };
 
 }
 
