1c7ca7e782d587b255324be873f6f60641d7c18c
Roldak <romain-be@hotmail.com>
2015-09-06 19:37:18 +0200

    updates doc

--- src/Compiler/Frontend/Types/TypeParametrizable.h
+++ src/Compiler/Frontend/Types/TypeParametrizable.h
@@ -19,13 +19,23 @@ class TypeSymbol;
 
 namespace type {
 
+/**
+ * @brief Interface which represents an object that can depend on type symbols.
+ * Concrete implementations are: ClassDecl, TraitDecl, TypeConstructorCreation and FunctionCreation.
+ */
 class TypeParametrizable {
 public:
     TypeParametrizable();
     virtual ~TypeParametrizable();
 
+    /**
+     * @param type The additionnal dependency of this object
+     */
     void setDependsOn(sym::TypeSymbol* type);
 
+    /**
+     * @return All the types that this object depends on
+     */
     const std::set<sym::TypeSymbol*>& getDependencies() const;
 
 private:
