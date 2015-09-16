fd4382e563649661d43741501f236f24304b6598
Romain <romain.beguet@epfl.ch>
2015-09-16 14:20:27 +0200

    ASTTypeCreator now inherits from ASTExplicitVisitor instead of ASTImplicitVisitor

--- src/Compiler/Frontend/AST/Visitors/ASTTypeCreator.cpp
+++ src/Compiler/Frontend/AST/Visitors/ASTTypeCreator.cpp
@@ -15,7 +15,7 @@ namespace sfsl {
 namespace ast {
 
 ASTTypeCreator::ASTTypeCreator(CompCtx_Ptr& ctx, const std::vector<type::Type*>& args)
-    : ASTImplicitVisitor(ctx), _created(nullptr), _args(args) {
+    : ASTExplicitVisitor(ctx), _created(nullptr), _args(args) {
 
 }
 
