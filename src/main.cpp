df3a7a4e489c61c1be112c8aae1930dee45f60f3
Romain <romain.beguet@epfl.ch>
2015-10-16 15:20:00 +0200

    closes #53 (+400th commit)

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,9 @@
 #include <sstream>
 #include <vector>
 #include <ctime>
-#include <memory.h>
+#include <memory>
 #include "unistd.h"
-#include "Compiler/Frontend/Lexer/Lexer.h"
-#include "Compiler/Frontend/Parser/Parser.h"
-#include "Compiler/Frontend/AST/Visitors/ASTPrinter.h"
-#include "Compiler/Frontend/Analyser/NameAnalysis.h"
-#include "Compiler/Frontend/Analyser/KindChecking.h"
-#include "Compiler/Frontend/Analyser/TypeChecking.h"
-#include "Compiler/Frontend/Symbols/SymbolResolver.h"
-#include "Compiler/Backend/UserDataAssignment.h"
-#include "Compiler/Backend/BytecodeGenerator.h"
+#include "sfsl.h"
 
 #define DEFAULT_CHUNK_SIZE 2048
 
@@ -51,98 +43,31 @@ int main(int argc, char** argv) {
 
     clock_t exec = clock();
 
-    auto ctx = common::CompilationContext::DefaultCompilationContext(DEFAULT_CHUNK_SIZE);
-    std::istringstream input(source);
-
-    src::IStreamSource src(src::InputSourceName::make(ctx, sourceFile), input);
+    Compiler cmp(CompilerConfig(StandartReporter::CerrReporter));
+    ByteCodeCollector bcc;
+    EmptyCollector emc;
+    AbstractOutputCollector* col;
+    Pipeline ppl = Pipeline::createDefault();
 
     try {
-
-        lex::Lexer lexer(ctx, src, 1024);
-
-        ast::Parser parser(ctx, lexer);
-
-        std::cout << "STARTING PARSER" << std::endl;
-        std::cout << ctx->memoryManager().getInfos() << std::endl << std::endl;
-
-        ast::Program* prog = parser.parse();
-
-        if (ctx->reporter().getErrorCount() != 0) {
-            return 1;
-        }
-
-        /*ast::ASTPrinter printer(ctx, std::cout);
-        prog->onVisit(&printer);*/
-
-        std::cout << "STARTING NAME ANALYSIS" << std::endl;
-        std::cout << ctx->memoryManager().getInfos() << std::endl << std::endl;
-
-        ast::ScopeGeneration scopeGen(ctx);
-        ast::TypeDependencyFixation typeDep(ctx);
-        ast::SymbolAssignation symAssign(ctx);
-
-        prog->onVisit(&scopeGen);
-        prog->onVisit(&typeDep);
-        prog->onVisit(&symAssign);
-
-        if (ctx->reporter().getErrorCount() != 0) {
-            return 1;
-        }
-
-        std::cout << "STARTING KINDCHECKING" << std::endl;
-        std::cout << ctx->memoryManager().getInfos() << std::endl << std::endl;
-
-        ast::KindChecking kindCheck(ctx);
-        prog->onVisit(&kindCheck);
-
-        if (ctx->reporter().getErrorCount() != 0) {
-            return 1;
-        }
-
-        sym::SymbolResolver res(prog, ctx);
-        res.setPredefClassesPath("sfsl.lang");
-
-        std::cout << "STARTING TYPECHECKING" << std::endl;
-        std::cout << ctx->memoryManager().getInfos() << std::endl << std::endl;
-
-        ast::TopLevelTypeChecking topleveltypecheck(ctx, res);
-        ast::TypeChecking typeCheck(ctx, res);
-
-        prog->onVisit(&topleveltypecheck);
-        prog->onVisit(&typeCheck);
-
-        if (ctx->reporter().getErrorCount() != 0) {
-            return 1;
-        }
+        ProgramBuilder prog = cmp.parse(sourceFile, source);
 
         if (checkOnly) {
-            return 0;
+            col = &emc;
+            ppl.insert(Phase::StopRightBefore("CodeGen"));
+        } else {
+            col = &bcc;
         }
 
-        std::cout << "STARTING BYTECODE GENERATION" << std::endl;
-        std::cout << ctx->memoryManager().getInfos() << std::endl << std::endl;
+        cmp.compile(prog, *col, ppl);
 
-        out::LinkedListOutput<bc::BCInstruction*> out(ctx);
-        bc::UserDataAssignment uda(ctx);
-        bc::DefaultBytecodeGenerator gen(ctx, out);
-
-        prog->onVisit(&uda);
-        prog->onVisit(&gen);
-
-        std::cout << "DONE" << std::endl;
-        std::cout << ctx->memoryManager().getInfos() << std::endl << std::endl;
-
-        for (const bc::BCInstruction* i : out.toVector()) {
-            std::cout << *i << std::endl;
-        }
-
-        if (ctx->reporter().getErrorCount() != 0) {
-            return 1;
+        for (const std::string& i : bcc.get()) {
+            std::cout << i << std::endl;
         }
 
         std::cout << "Compilation Time : " << (clock() - exec)/(double)CLOCKS_PER_SEC << std::endl << std::endl;
 
-    } catch(const sfsl::common::CompilationFatalError& ex) {
+    } catch(const CompileError& ex) {
         std::cerr << ex.what() << std::endl;
     }
 }
