3848d51269f3c0c09735e1b91012dbd70323a5a0
Roldak <romain-be@hotmail.com>
2016-09-22 00:12:23 +0200

    tries to make it work

--- tests/CanSubtypeTests.cpp
+++ tests/CanSubtypeTests.cpp
@@ -8,6 +8,7 @@
 
 #include <fstream>
 #include <algorithm>
+#include <sstream>
 
 #include "sfsl.h"
 #include "CanSubtypeTests.h"
@@ -32,12 +33,18 @@ private:
     std::string _name;
 };
 
+std::string charToString(char c) {
+    std::stringstream ss;
+    ss << c;
+    return ss.str();
+}
+
 class SubTypingTest final : public AbstractTest {
 public:
     SubTypingTest(const std::string& name, size_t clazzCount)
-        : AbstractTest(name), _clazzes(clazzCount), _success(true) {
+        : AbstractTest(name), _clazzes(clazzCount), _success(true), _assertionsPrepared(false) {
         for (size_t i = 0; i < clazzCount; ++i) {
-            _clazzes[i] = new Clazz(std::to_string(nameFrom(i)));
+            _clazzes[i] = new Clazz(charToString(nameFrom(i)));
             _clazzes[i]->addSpecialSuperType(_clazzes[i], Environment::Empty);
         }
     }
@@ -52,6 +59,10 @@ public:
     }
 
     SubTypingTest* subs(char a, char b) {
+        if (_assertionsPrepared) {
+            throw new std::runtime_error("Cannot add subtyping relations when assertions are already prepared");
+        }
+
         clazz(a)->addSuperType(clazz(b), Environment::Empty);
         _log += a;
         _log += " <! ";
@@ -61,6 +72,10 @@ public:
     }
 
     SubTypingTest* specialSubs(char a, char b) {
+        if (_assertionsPrepared) {
+            throw new std::runtime_error("Cannot add subtyping relations when assertions are already prepared");
+        }
+
         clazz(a)->addSpecialSuperType(clazz(b), Environment::Empty);
         _log += a;
         _log += " <-! ";
@@ -69,7 +84,23 @@ public:
         return this;
     }
 
+    SubTypingTest* prepareAssertions() {
+        if (_assertionsPrepared) {
+            throw new std::runtime_error("Assertions already prepared");
+        }
+
+        _assertionsPrepared = true;
+
+        for (Clazz* clazz : _clazzes) {
+        }
+        return this;
+    }
+
     SubTypingTest* assertIsSubtype(char a, char b) {
+        if (!_assertionsPrepared) {
+            throw new std::runtime_error("Cannot add assertions while assertions are not prepared");
+        }
+
         bool subtypes = !clazz(a)->subTypeInstances(clazz(b)).empty();
         _log += a;
         _log += " <? ";
@@ -85,6 +116,10 @@ public:
     }
 
     SubTypingTest* assertIsNotSubtype(char a, char b) {
+        if (!_assertionsPrepared) {
+            throw new std::runtime_error("Cannot add assertions while assertions are not prepared");
+        }
+
         bool subtypes = !clazz(a)->subTypeInstances(clazz(b)).empty();
         _log += a;
         _log += " not <? ";
@@ -125,15 +160,24 @@ private:
     std::vector<Clazz*> _clazzes;
     std::string _log;
     bool _success;
+    bool _assertionsPrepared;
 };
 
 TestRunner* buildCanSubtypeTests() {
     TestSuiteBuilder basic("Basic");
 
-    basic.addTest(SubTypingTest::make("Subtype of self", 1)->assertIsSubtype('A', 'A'));
-    basic.addTest(SubTypingTest::make("Basic #1", 2)->subs('A', 'B')->assertIsSubtype('A', 'B'));
+    basic.addTest(SubTypingTest::make("Subtype of self", 1)->prepareAssertions()->assertIsSubtype('A', 'A'));
+    basic.addTest(SubTypingTest::make("Basic #1", 2)->subs('A', 'B')->prepareAssertions()->assertIsSubtype('A', 'B'));
 
-    return new TestRunner("CanSubtypeTests", {basic.build()});
+    TestSuiteBuilder trans("Transitivity");
+
+    trans.addTest(SubTypingTest::make("Transitivity #1", 3)->subs('A', 'B')->subs('B', 'C')
+                  ->prepareAssertions()->assertIsSubtype('A', 'C'));
+
+    trans.addTest(SubTypingTest::make("Transitivity #2", 3)->subs('B', 'C')->subs('A', 'B')
+                  ->prepareAssertions()->assertIsSubtype('A', 'C'));
+
+    return new TestRunner("CanSubtypeTests", {basic.build(), trans.build()});
 }
 
 }
