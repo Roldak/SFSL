c631123dda806e39a43fc43829ec63b5f1985587
Romain <romain.beguet@epfl.ch>
2016-11-11 20:56:44 +0100

    fixes capture tests and adds some

--- tests/Visitors/CapturesChecker.cpp
+++ tests/Visitors/CapturesChecker.cpp
@@ -38,19 +38,35 @@ void CapturesChecker::visit(ast::FunctionCreation* func) {
 void CapturesChecker::visitClosure(const common::Positionnable& pos, ast::Annotable* annotable,
                                    common::HasManageableUserdata* closureData) {
 
+    std::vector<std::pair<std::string, bool>> expected;
+
+    annotable->matchAnnotation<std::string, bool>("captures", [&](std::string name, bool mut) {
+        expected.push_back(std::make_pair(name, mut));
+    });
+
     if (ast::ClassPatch* patch = closureData->getUserdata<ast::ClassPatch>()) {
         for (const ast::Change& c : patch->getChanges()) {
             bool ok = false;
 
-            annotable->matchAnnotation<std::string, bool>("captures", [&](std::string name, bool mut) {
-                if (c.newField->getValue() == name) {
+            for (auto candidate = expected.begin(); candidate != expected.end();) {
+                if (c.newField->getValue() == candidate->first) {
+                    candidate = expected.erase(candidate);
                     ok = true;
+                    break;
+                } else {
+                    ++candidate;
                 }
-            });
+            }
 
             if (!ok) {
                 _ctx->reporter().error(pos, c.newField->getValue() + " is captured but not checked");
             }
+        }
+    }
+
+    if (expected.size() > 0) {
+        for (const auto& candidate : expected) {
+            _ctx->reporter().error(pos, candidate.first + " was expected to be captured but is not");
         }
     }
 }
